4377043661f0f3abfed61f3934e99e809fa891f5
Mykola Holovanchuk <k.holovanchuk@icloud.com>
2022-09-09 18:55:38 +0200

    first steps

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp1 import views as myapp_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', myapp_views.index, name='index'),
 ]
